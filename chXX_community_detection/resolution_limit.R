38ae74d20074fea7c985464feb729fc3c6480c63
kztakemoto <kz.takemoto@gmail.com>
2020-03-20 16:57:45 +0900

    updated

--- chXX_community_detection/resolution_limit.R
+++ chXX_community_detection/resolution_limit.R
@@ -31,21 +31,21 @@ cols <- brewer.pal(9,"Set3")
 mem <- c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6) # 割り当て
 modularity(g_6, mem)
 set.seed(123)
-plot(g_6, vertex.color=cols[mem], main=paste("Q=",modularity(g_6, mem),sep=""))
+plot(g_6, vertex.color=cols[mem], vertex.label=NA, main=paste("Q=",modularity(g_6, mem),sep=""))
 
 mem <- c(1,1,1,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5) # 割り当て
 modularity(g_6, mem)
 set.seed(123)
-plot(g_6, vertex.color=cols[mem], main=paste("Q=",modularity(g_6, mem),sep=""))
+plot(g_6, vertex.color=cols[mem], vertex.label=NA, main=paste("Q=",modularity(g_6, mem),sep=""))
 
 ## コミュニティの数が閾値（8）より大きい場合、コミュニティを見逃す
 ##（コミュニティが併合した方がモジュラリティQが大きくなる）。
 mem <- c(1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9,9,9) # 割り当て
 modularity(g_9, mem)
 set.seed(123)
-plot(g_9, vertex.color=cols[mem], main=paste("Q=",modularity(g_9, mem),sep=""))
+plot(g_9, vertex.color=cols[mem], vertex.label=NA, main=paste("Q=",modularity(g_9, mem),sep=""))
 
 mem <- c(1,1,1,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8) # 割り当て
 modularity(g_9, mem)
 set.seed(123)
-plot(g_9, vertex.color=cols[mem], main=paste("Q=",modularity(g_9, mem),sep=""))
+plot(g_9, vertex.color=cols[mem], vertex.label=NA, main=paste("Q=",modularity(g_9, mem),sep=""))
